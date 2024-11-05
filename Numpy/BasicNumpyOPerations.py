af3621e52e7b94f05597dc5386ac71dbf35950ee
shruthi.s4 <shruthi.s4@gmail.com>
2024-11-05 06:05:22 -0500

    Numpy

---
+++ Numpy/BasicNumpyOPerations.py
@@ -0,0 +1,8 @@
+import numpy as np
+# this creates a s 3-d array. You can identifiy by the number of sqaure brackets
+a = np.array([[[1,2],[3,4]]])
+print(a)
+print(a.shape)
+print(a.ndim)
+
+# referencing a numpy array
